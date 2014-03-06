9a56267d6a2d9b774872d9635e0c2ed27555c6c1
cwiedel <chrillan@gmail.com>
2014-03-06 20:29:54 +0100

    Initial Commit

---
+++ SimpleGamePIA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleGamePIA
+//
+//  Created by Christian Wiedel on 06/03/14.
+//  Copyright (c) 2014 Christian Wiedel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleGamePIA/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleGamePIA
+//
+//  Created by Christian Wiedel on 06/03/14.
+//  Copyright (c) 2014 Christian Wiedel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
